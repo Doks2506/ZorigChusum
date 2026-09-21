fae66db0ff63654a8e7cbad0d20c0d26f7c362ca
Doks2506 <tshedoll806@gmail.com>
2026-09-21 14:04:00 +0600

    Initial commit

---
+++ ZorigChusum/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZorigChusum
+//
+//  Created by iMac15 on 9/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZorigChusum/ZorigChusumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZorigChusumApp.swift
+//  ZorigChusum
+//
+//  Created by iMac15 on 9/21/26.
+//
+
+import SwiftUI
+
+@main
+struct ZorigChusumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
